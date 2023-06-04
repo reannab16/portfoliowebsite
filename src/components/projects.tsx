b1279a5e950a941461e80049f678bba303699611
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-06-04 21:16:13 +0100

    projects responsive

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -50,19 +50,19 @@ export default function Projects() {
     return(
         <div className="proj-contain-1 bg-[#cddfae] h-[100vh] w-[60vw] flex flex-col justify-center items-center max-w-[1280px] max-h-[70rem]" id="projects">
             <div className="proj-contain-2 h-full w-[80%] flex flex-col justify-center items-center">
-                <div className="proj-headers-contain flex flex-col justify-center items-center h-[20%] w-[91.875%]">
-                    <div className="projtext-contain flex flex-row justify-start items-center h-[60%] w-full">
-                        <div className="text-7xl font-extrabold text-[var(--darkGreen)] relative">
+                <div className="proj-headers-contain flex flex-col justify-center items-center h-[20%] lg:h-[15%] w-[91.875%]">
+                    <div className="flex flex-row justify-start items-center h-[60%] w-full">
+                        <div className="projtext-contain text-7xl font-extrabold text-[var(--darkGreen)] relative">
                             <LeafClove className="absolute h-[100%] -top-[4.3rem] left-[4.3rem]"/>
                             PROJECTS
-                            </div>
+                        </div>
                         <div className="proj-and text-5xl font-extrabold italic text-white ml-[2%]">&</div>
                     </div>
-                    <div className="arttext-contain flex flex-row justify-end items-center h-[40%] w-full">
-                        <div className="text-5xl font-extrabold text-[var(--lightPink)] relative">
+                    <div className=" flex flex-row justify-end items-center h-[40%] w-full">
+                        <div className="arttext-contain text-5xl font-extrabold text-[var(--lightPink)] relative">
                             <LeafStem className="absolute -top-10 h-12 left-[8.6rem] "/>
                             ARTWORK
-                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="projandart-contain flex flex-row h-[50%] w-[91.875%] justify-between items-center">
@@ -80,12 +80,12 @@ export default function Projects() {
                             )
                         })}
                     </div>
-                    <div className="artboxes flex flex-row justify-around items-around h-[100%] w-[41.9%] flex-wrap">
+                    <div className="artboxes-contain flex flex-row justify-around items-around h-[100%] w-[41.9%] flex-wrap">
                         {Artwork.map((item)=>{
                             return(
                                 <a
                                     key={item.name}
-                                    className="bg-[var(--lightPink)] h-[41%] w-[48%] mb-[-1.5%] relative flex flex-col items-center justify-center"
+                                    className="artboxes bg-[var(--lightPink)] h-[41%] w-[48%] mb-[-1.5%] relative flex flex-col items-center justify-center"
                                     href="">
                                         <img src={item.src} alt={item.name} className="h-[100%] w-[100%] object-cover"/>
                                         <div className="bg-[var(--darkGreen)] h-full w-full z-30 absolute top-0 bottom-0 opacity-70 hover:opacity-0"></div>
