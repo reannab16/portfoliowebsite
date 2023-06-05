d3e4a13cf57696be5d0d9569ae4a6c40d317b4ea
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-06-05 02:44:31 +0100

    quickfix

--- src/components/sideemail.tsx
+++ src/components/sideemail.tsx
@@ -5,7 +5,7 @@ export default function SideEmail() {
     <div className="side-email duration-300 max-[820px]:opacity-0">
       <a
         href="mailto:contact@reanna.dev"
-        className="vertical-text text-sm tracking-[0.2em] text-[#65a888]"
+        className="vertical-text text-sm tracking-[0.2em] text-[#65a888] duration-300"
       >
         contact@reanna.dev
       </a>
