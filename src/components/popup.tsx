a81ea1ea38d8f9c91fd57b3b86a04311e6caffb2
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2023-06-24 02:55:37 +0100

    Make projects popup responsive

--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -23,62 +23,64 @@ export default function PopUp({ project, onClick }: PopUpProps) {
   //   }
 
   return (
-    <div className="fixed top-[calc(50%-15vh)] left-[calc(50%-25vw)] p-9 w-[50vw] z-10 flex flex-row justify-center items-center border-4 bg-[var(--lightGreen)] border-[var(--pandaLight)] rounded-xl">
-      <a
-        className="absolute top-0 right-0 p-2 hover:cursor-pointer"
-        onClick={onClick}
-      >
-        <FontAwesomeIcon
-          icon={faSquareXmark}
-          className="text-[var(--pandaLight)] h-5 ml-2 hover:text-[var(--darkGreen)] duration-300"
-        />
-      </a>
-      <img
-        src={project?.popupsrc}
-        alt="first portfolio picture"
-        className=" w-[30%] object-contain rounded-xl ml-5"
-      />
-      <div className="w-[70%] flex flex-col justify-between items-start mr-5">
-        <div className="font-extrabold text-[var(--pandaLight)] text-4xl ml-5 flex items-center justify-center">
-          <div>{project!.title}</div>
-          <a
-            href={project?.projectsrc}
-            target="blank"
-            className="flex items-center justify-center"
-          >
+    <div className="fixed z-10 h-full w-full top-0 left-0 flex items-center justify-center ">
+        <div className="relative p-9 xl:w-[50vw] lg:w-[60vw] md:w-[70vw] sm:w-[80vw] w-[90vw]  flex flex-row justify-center items-center border-4 bg-[var(--lightGreen)] border-[var(--pandaLight)] rounded-xl">
+        <a
+            className="absolute top-0 right-0 p-2 hover:cursor-pointer"
+            onClick={onClick}
+        >
             <FontAwesomeIcon
-              icon={faArrowUpRightFromSquare}
-              className="h-5 ml-2 hover:text-[var(--darkGreen)] duration-300 cursor-pointer hover:-translate-y-[2px]"
+            icon={faSquareXmark}
+            className="text-[var(--pandaLight)] h-5 ml-2 hover:text-[var(--darkGreen)] duration-300"
             />
-          </a>
-
-          {project?.git && (
+        </a>
+        <img
+            src={project?.popupsrc}
+            alt="first portfolio picture"
+            className=" w-[30%] object-contain rounded-xl sm:ml-5 ml-2"
+        />
+        <div className="w-[70%] flex flex-col justify-between items-start sm:mr-5 mr-2">
+            <div className="font-extrabold text-[var(--pandaLight)] xl:text-4xl sm:text-3xl text-2xl ml-5 flex items-center justify-center">
+            <div>{project!.title}</div>
             <a
-              href={project?.git}
-              target="blank"
-              className="flex items-center justify-center"
+                href={project?.projectsrc}
+                target="blank"
+                className="flex items-center justify-center"
             >
                 <FontAwesomeIcon
-                  icon={faGithub}
-                  className="h-5 ml-2 hover:text-[var(--darkGreen)] duration-300 cursor-pointer hover:-translate-y-[2px]"
+                icon={faArrowUpRightFromSquare}
+                className="sm:h-5 h-4 ml-2 hover:text-[var(--darkGreen)] duration-300 cursor-pointer hover:-translate-y-[2px]"
                 />
             </a>
-          )}
-        </div>
-        <div className="text-[var(--pandaLight)] text-base ml-5 mt-2">
-          {project!.text}
-        </div>
 
-        <div className="ml-5 flex flex-row items-center justify-start flex-wrap mt-2">
-          {project?.skills.map((skill) => {
-            return (
-              <div className="bg-[var(--darkGreen)] flex items-center justify-center pr-4 pl-4 p-1 rounded-full text-slate-100 text-base m-2">
-                {skill}
-              </div>
-            );
-          })}
+            {project?.git && (
+                <a
+                href={project?.git}
+                target="blank"
+                className="flex items-center justify-center"
+                >
+                    <FontAwesomeIcon
+                    icon={faGithub}
+                    className="sm:h-5 h-4 ml-2 hover:text-[var(--darkGreen)] duration-300 cursor-pointer hover:-translate-y-[2px]"
+                    />
+                </a>
+            )}
+            </div>
+            <div className="text-[var(--pandaLight)] text-base ml-5 mt-2">
+            {project!.text}
+            </div>
+
+            <div className="ml-5 flex flex-row items-center justify-start flex-wrap mt-2">
+            {project?.skills.map((skill) => {
+                return (
+                <div className="bg-[var(--darkGreen)] flex items-center justify-center pr-4 pl-4 p-1 rounded-full text-slate-100 xl:text-base text-sm xl:m-2 m-1">
+                    {skill}
+                </div>
+                );
+            })}
+            </div>
         </div>
-      </div>
+        </div>
     </div>
   );
 }
